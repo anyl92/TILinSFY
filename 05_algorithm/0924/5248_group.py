e9b63b20d05ee9436512eaa81cad47dbc6e1328b
yulim <yulim9592@gmail.com>
2019-09-24 19:24:33 +0900

    190924

--- 05_algorithm/0924/5248_group.py
+++ 05_algorithm/0924/5248_group.py
@@ -1,31 +1,29 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
+def find(i):
+    if V[i] == 0:
+        V[i] = cnt
+    for j in range(1, N+1):
+        if L[i][j] == 1 and V[j] == 0:
+            find(j)
+
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    L = list(map(int, input().split()))
+    G = list(map(int, input().split()))
+    L = [[0] * (N+1) for _ in range(N+1)]
 
-    G = [[] for _ in range(N+1)]
-    for i in range(len(L)//2):
-        G[L[i * 2]].append(L[i * 2 + 1])
-        G[L[i * 2 + 1]].append(L[i * 2])
+    while G:
+        a = G.pop(0)
+        b = G.pop(0)
+        L[a][b] = 1
+        L[b][a] = 1
 
-    C = []
-    for i in range(1, len(G)):
-        G[i] += [i]
-        C.append(G[i])
-    print('C', C)
-
-    res = []
+    V = [0] * (N+1)
+    cnt = 1
     for i in range(1, N+1):
-        tmp = []
-        for j in C:
-            if i in j:
-                if sorted(j) not in tmp:
-                    tmp.append(sorted(j))
-        print('tmp', tmp)
-        # if tmp not in res:
-        #     res += tmp
-        # print(res)
+        find(i)
+        cnt += 1
 
+    print('#%d %d' % (tc, len(set(V))-1))
