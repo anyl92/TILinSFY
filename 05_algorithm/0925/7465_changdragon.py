5298ce46df58f49effef726d2d7700eeab085dc5
yulim <yulim9592@gmail.com>
2019-09-25 15:43:14 +0900

    190925

--- 05_algorithm/0925/7465_changdragon.py
+++ 05_algorithm/0925/7465_changdragon.py
@@ -5,5 +5,23 @@ T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
     L = [list(map(int, input().split())) for _ in range(M)]
-    print(N, M, L)
 
+    for i in range(len(L)):
+        if L[i]:
+            for j in range(len(L)):
+                if i != j and L[j]:
+                    for k in L[j]:
+                        if k in L[i]:
+                            L[i] += L[j]
+                            L[i] = list(set(L[i]))
+                            L[j] = []
+                            break
+    cnt = 0
+    per = []
+    for i in L:
+        if i:
+            cnt += 1
+        per += i
+    res = N - len(per) + cnt
+
+    print('#%d %d' % (tc, res))
--- 05_algorithm/0925/back_14889_teacher's(perm).py
+++ 05_algorithm/0925/back_14889_teacher's(perm).py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('input.txt', 'r')
+sys.stdin = open('14889_input.txt', 'r')
 
 
 def calc(T):
@@ -18,24 +18,49 @@ def calc(T):
     return tmp
 
 
-def perm(k):
+# def perm(k):
+#     global mini
+#     if k == N//2:
+#         print(T)
+#         tmp = calc(T)
+#         if mini > tmp:
+#             mini = tmp
+#         return
+#
+#     for i in range(N//2, N):
+#
+#         T[k], T[i] = T[i], T[k]
+#         perm(k + 1)
+#         T[k], T[i] = T[i], T[k]
+
+
+def perm(k, m):
     global mini
-    if k >= N:
+    if k == N//2:
+        print(T)
         tmp = calc(T)
         if mini > tmp:
             mini = tmp
         return
 
-    for i in range(k, N):
+    for i in range(m, N):
         T[k], T[i] = T[i], T[k]
-        perm(k + 1)
+        perm(k + 1, i)
         T[k], T[i] = T[i], T[k]
-
 
 N = int(input())
 L = [list(map(int, input().split())) for _ in range(N)]
 T = [i for i in range(N)]
-mini = 999999
+mini = calc(T)
 
-perm(0)
+# perm(0)
+perm(0, N//2)
 print(mini)
+
+# print(T)
+# start = []
+# for i in range(len(T)):
+#     for j in range(1+i, len(T)):
+#         start += [T[i]]
+#         start += [T[j]]
+#         print(start)
